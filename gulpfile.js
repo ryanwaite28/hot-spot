b50568d5969420a556da6552b421f424cccf3f16
Ryan M. Waite <ryanwaite28@gmail.com>
2018-07-20 10:57:20 -0400

    updated: 7/20/2018 - 10:57 AM EST

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var uglifyes = require('gulp-uglifyes');
 var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var gutil = require('gulp-util');
+var htmlmin = require('gulp-htmlmin');
 var del = require('del');
 
 var paths = {
@@ -17,6 +18,10 @@ var paths = {
   scripts: {
     src: 'src/origin/js/**/*.js',
     dest: 'src/vault/js/utils/'
+  },
+  pages: {
+    src: 'src/origin/html/**/*.html',
+    dest: 'src/vault/html/'
   }
 };
 
@@ -40,23 +45,35 @@ function scripts() {
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
+function pages() {
+  return gulp.src(paths.pages.src)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+    .pipe(gulp.dest(paths.pages.dest));
+}
+
 function watch() {
   gulp.watch(paths.scripts.src, scripts);
   gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.pages.src, pages);
 }
 
+
+
 function build() {
-  return gulp.series(gulp.parallel(styles, scripts));
+  return gulp.series(gulp.parallel(styles, scripts, pages));
 }
 
 /* --- */
 
 exports.styles = styles;
 exports.scripts = scripts;
+exports.pages = pages;
 exports.watch = watch;
 
 gulp.task('styles', styles);
 gulp.task('scripts', scripts);
+gulp.task('pages', pages);
 gulp.task('watch', watch);
 
 gulp.task('build', build);
